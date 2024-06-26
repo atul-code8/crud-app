6db56f9741d3b4d6fa17bdad15b7db586e8eaa7a
Atul Ghormare <atulghormare6@gmail.com>
2024-06-26 21:51:12 +0530

    modified index.js, .gitignore

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-dotenv.config({path: '.env.local'});
 dotenv.config();
 
 // Create an Express application
@@ -14,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.DB_CONNECT_URI , {
+mongoose.connect(process.env.MONGODB_URI , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -59,7 +58,8 @@ app.delete("/items/:id", async (req, res) => {
   res.json({ message: "Item deleted" });
 });
 
+const port = process.env.PORT || 5000;
 // Start the server
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
